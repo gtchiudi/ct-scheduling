6f68d9f7e83a1c0709a7f814d0325c10010b2e86
Gino Chiudioni <gchiudio@kent.edu>
2023-07-17 20:34:59 -0400

    added fields to class Request and added active boolean to all tables.

--- server/members/models.py
+++ server/members/models.py
@@ -5,14 +5,27 @@ from django.db import models
 class Request(models.Model):
     request_id = models.IntegerField(primary_key=True, default=0)
     approved = models.BooleanField(default=False)
-    company_name = models.CharField(max_length=255)
-    phone_number = models.CharField(max_length=12)
-    po_number = models.IntegerField(default=0)
-    container_number = models.IntegerField(default=0)
-    outbound_number = models.IntegerField(default=0)
-    reference_number = models.IntegerField(default=0)
-    note_section = models.CharField(max_length=512)
-    date_time = models.DateTimeField("Request Date")
+    company_name = models.CharField(
+        max_length=255)  # changeable only via admin
+    phone_number = models.CharField(
+        max_length=12, null=True)  # changeable only via admin
+    email = models.CharField(max_length=255)  # changeable only via admin
+    po_number = models.IntegerField(default=0)  # changable via emp
+    load_type = models.CharField(max_length=32)  # changable via emp
+    container_number = models.IntegerField(default=0)  # changeable via emp
+    note_section = models.CharField(
+        max_length=512, null=True)  # changable via emp
+    date_time = models.DateTimeField("Request Date")  # changable via emp
+    delivery = models.BooleanField(default=False)  # changable via emp
+    trailer_number = models.IntegerField(default=0)  # employee use only
+    driver_phone_number = models.CharField(
+        max_length=12, null=True)  # emp use only
+    dock_number = models.IntegerField(default=0)  # emp use only
+    check_in_time = models.TimeField("Checked In", null=True)  # emp use only
+    docked_time = models.TimeField("Docked Time", null=True)  # emp use only
+    completed_time = models.TimeField(
+        "Time Completed", null=True)  # emp use only
+    active = models.BooleanField(default=True)
 
 
 class Employee(models.Model):
@@ -20,11 +33,13 @@ class Employee(models.Model):
     email = models.CharField(max_length=255)
     phone_number = models.CharField(max_length=12)
     password = models.CharField(max_length=32)
+    active = models.BooleanField(default=True)
 
 
 class Role(models.Model):
     emp_id = models.ForeignKey(Employee, on_delete=models.CASCADE)
     role = models.CharField(max_length=32)
+    active = models.BooleanField(default=True)
 
 
 class Warehouse(models.Model):
@@ -32,12 +47,14 @@ class Warehouse(models.Model):
     name = models.CharField(max_length=64)
     address = models.CharField(max_length=256)
     phone_number = models.CharField(max_length=12)
+    active = models.BooleanField(default=True)
 
 
 class Actions_Log(models.Model):
     emp_id = models.ForeignKey(Employee, on_delete=models.CASCADE)
     action = models.CharField(max_length=256)
     date_time = models.DateTimeField("Action Date/Time")
+    active = models.BooleanField(default=True)
 
 
 class Schedule(models.Model):
@@ -46,3 +63,4 @@ class Schedule(models.Model):
         Request, on_delete=models.CASCADE)
     warehouse_id = models.ForeignKey(
         Warehouse, on_delete=models.CASCADE)
+    active = models.BooleanField(default=True)
