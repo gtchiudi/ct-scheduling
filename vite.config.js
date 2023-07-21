3254fe6abe0dea72bb03e1f017a4c50598e68365
Gino Chiudioni <gchiudio@kent.edu>
2023-07-21 11:52:43 -0400

    fixed issues in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['react', 'react-dom'],
-  },
+  
   build: {
     manifest: true,
     rollupOptions: {
