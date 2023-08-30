c753dacde52c714334cb201dec02936653977ca7
Gino Chiudioni <gchiudio@kent.edu>
2023-08-30 14:56:05 -0400

    vite.config.js change. Altered the target URL for the server proxy to a pythonanywhere host.

--- vite.config.js
+++ vite.config.js
@@ -15,7 +15,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-          target: 'http://localhost:8000',
+          target: 'http://gtchiudi.pythonanywhere.com',
           changeOrigin: true,
       }
     }
