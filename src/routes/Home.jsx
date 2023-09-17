bba3d94bd39783240903d4f015614fbee826a4ab
khuship47 <98182634+khuship47@users.noreply.github.com>
2023-09-17 18:14:24 -0400

    home page update

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -3,7 +3,10 @@ import * as React from "react";
 export default function Home(){
     return(
         <div>
-            <h1>Hello World!</h1>
+            <h1 style={{textAlign:"center"}}>Welcome to Candor Logistics</h1>
+            <img src="../truckBackground.png" style={{ width: "100%", height: "100%" }}></img>
+            <button style={{color:"blue"}}>Request Pickup/Delivery</button>
         </div>
+    
     )
 }
