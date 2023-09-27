8366c25e35d0c5cef3fae03f64f3764577ff8c42
khuship47 <98182634+khuship47@users.noreply.github.com>
2023-09-27 17:24:51 -0400

    sx

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,12 +1,27 @@
 import * as React from "react";
+import { Box, ThemeProvider, createTheme } from '@mui/system';
+import { Typography } from "@mui/material";
 
 export default function Home(){
     return(
-        <div>
-            <h1 style={{textAlign:"center"}}>Welcome to Candor Logistics</h1>
-            <img src="../truckBackground.png" style={{ width: "100%", height: "100%" }}></img>
-            <button style={{color:"blue"}}>Request Pickup/Delivery</button>
-        </div>
+        <Typography>
+        <Box
+        sx={{
+            color: '',
+            display: 'inline',
+            fontWeight: 'bold',
+            textAlign: 'center',
+            mx: 0.5,
+            fontSize: 14,
+          }}><h1>Welcome to Candor Logistics</h1>
+          </Box>
     
+         <img style={{height: '100%',
+          width: '100%',}}src="../truckBackground.png"></img>
+
+        <div>
+            <button>Request Pickup/Delivery</button>
+        </div>
+    </Typography>
     )
 }
