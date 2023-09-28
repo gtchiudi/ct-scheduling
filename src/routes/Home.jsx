e47cacfe7e348d6982ba921f9cda6bdd852106bb
khuship47 <98182634+khuship47@users.noreply.github.com>
2023-09-28 10:19:12 -0400

    update to button

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import { Box, ThemeProvider, createTheme } from '@mui/system';
 import { Typography } from "@mui/material";
+import Button from "@mui/material/Button";
 
 export default function Home(){
     return(
@@ -14,14 +15,25 @@ export default function Home(){
             mx: 0.5,
             fontSize: 14,
           }}><h1>Welcome to Candor Logistics</h1>
-          </Box>
-    
+        </Box>
+        
          <img style={{height: '100%',
           width: '100%',}}src="../truckBackground.png"></img>
-
-        <div>
-            <button>Request Pickup/Delivery</button>
-        </div>
+        
+        <Box sx={{ textAlign: 'center' }}>
+            <Button
+                variant="outlined"
+                size="medium"
+                sx={{
+                    textAlign: "center",
+                    "&:hover": {
+                    backgroundColor: "#AAC1D0",
+                    opacity: [0.9, 0.8, 0.7]
+                    }
+                }}>
+                Request Pickup/Delivery
+            </Button>
+        </Box>
     </Typography>
     )
 }
