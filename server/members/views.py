43292e50f8fe7defeda8841e2afd5f9f2f8ab912
Gino Chiudioni <gchiudio@kent.edu>
2023-11-03 15:42:03 -0400

    filter out inactive rows

--- server/members/views.py
+++ server/members/views.py
@@ -34,11 +34,12 @@ class RequestView(viewsets.ModelViewSet):
         end_date = self.request.query_params.get('end_date')
         active = self.request.query_params.get('active')
 
-        if isApproved and active:
+        if isApproved:
             queryset = queryset.filter(approved=isApproved)
             if start_date and end_date:
                 queryset = queryset.filter(
                     date_time__gte=start_date, date_time__lte=end_date)
+        queryset = queryset.filter(active=True)
         return queryset
 
     def put(self, request, pk, format=None):
