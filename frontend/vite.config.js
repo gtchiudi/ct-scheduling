3420d38a044ed28bdaf8ecd22c0c3b88bda12627
Gino Chiudioni <gchiudio@kent.edu>
2026-03-21 10:28:22 -0400

    settings changes for production

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,7 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 
-const proxyURL = 'http://localhost:8000'; //''; //
+const debug = false
+const proxyURL = debug ? 'http://localhost:8000' : '';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -22,26 +23,22 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-          // target: process.env.proxyURL,
-          target: proxyURL,
-          changeOrigin: true,
-          secure: false,
+        target: debug ? proxyURL : process.env.proxyURL,
+        changeOrigin: true,
+        secure: false,
       },
       '/token/': {
-          // target: process.env.proxyURL,
-          target: proxyURL,
-          changeOrigin: true,
-          secure: false,
+        target: debug ? proxyURL : process.env.proxyURL,
+        changeOrigin: true,
+        secure: false,
       },
       '/logout/': {
-          // target: process.env.proxyURL,
-          target: proxyURL,
-          changeOrigin: true,
-          secure: false,
+        target: debug ? proxyURL : process.env.proxyURL,
+        changeOrigin: true,
+        secure: false,
       },
       '/admin/': {
-        // target: process.env.proxyURL,
-        target: proxyURL,
+        target: debug ? proxyURL : process.env.proxyURL,
         changeOrigin: true,
         secure: false,
       }
