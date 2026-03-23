49e080764302d51bc225474f387566fbbfc6f863
Gino Chiudioni <gchiudio@kent.edu>
2026-03-22 22:25:20 -0400

    altered django debug and secret key to be in .env

--- backend/server/settings.py
+++ backend/server/settings.py
@@ -27,10 +27,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-q*+(03o^)&zw-)ve8a441iw-tnura(^qj#j&wgy8580*01a(6r'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-q*+(03o^)&zw-)ve8a441iw-tnura(^qj#j&wgy8580*01a(6r')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DJANGO_DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = ['*']
 
